a8414abaac0ef5fa26c1f5a98b88e59d01ff80a2
rvs-team <t109598097@ntut.org.tw>
2020-12-20 19:24:24 +0800

    💄 Added a property on DrawingBoard

--- src/App/component/DrawingBoard.jsx
+++ src/App/component/DrawingBoard.jsx
@@ -3,39 +3,31 @@ import { Line } from "react-chartjs-2"
 
 export default function DrawingBoard(props) {
   
-  // const [loading, setLoading] = useState(false);
-  // const handleClose = () => {
-  //   setLoading(false);
-  // };
-  // const handleToggle = () => {
-  //   setLoading(!open);
-  // };
-
     const [data, setData] = useState({})
 
     const getRandomColor = () => {
-      // var letters = "0123456789ABCDEF"
-      // var color = "#"
-      // for (var i=0 i<6 i++) {
-      //   color += letters[Math.floor(Math.random()*16)]
-      // }
-      // return color
       return `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
     }
 
     useEffect(() => {
       if (props.data) {
+        let _backgroundColor = getRandomColor()
+        if (props.color) {
+          _backgroundColor = `${props.color}`
+        }
         let datasets = Object.keys(props.data.data).map(key => {
           return {
             label: key,
             fill: "origin",
             borderColor: getRandomColor(),
+            backgroundColor: _backgroundColor,
             borderWidth: 1,
             pointRadius: 2,
-            data: props.data.data[key]
+            data: props.data.data[key],
+            tension: 0
           }
         })
-
+        
         setData({
           labels: props.data.labels,
           datasets: datasets
--- src/App/component/IssuePage.jsx
+++ src/App/component/IssuePage.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Card } from '@material-ui/core'
 import ProjectAvatar from './ProjectAvatar'
 import DrawingBoard from './DrawingBoard'
 import Axios from 'axios'
 import moment from 'moment'
-import CircularProgress from '@material-ui/core/CircularProgress'
+import { CircularProgress, Backdrop } from '@material-ui/core'
 import { connect } from 'react-redux';
 
 
@@ -17,6 +16,10 @@ const useStyles = makeStyles((theme) => ({
       },
       minWidth: '30px',
     },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
 }))
 
 function IssuePage(prop) {
@@ -24,19 +27,46 @@ function IssuePage(prop) {
   const [issueListData, setIssueListData] = useState([])
   const [dataForIssueChart, setDataForIssueChart] = useState({ labels:[], data: { created: [], closed: []} })
 
+  const [currentProject, setCurrentProject] = useState({ })
+
   const projectId = localStorage.getItem("projectId")
-  const projectName = localStorage.getItem("projectName")
-  const avatarURL = localStorage.getItem("avatarURL")
+
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
 
   useEffect(() => {
-    Axios.get("http://localhost:9100/pvs-api/issues/facebook/react")
-         .then((response) => {
-           setIssueListData(response.data)
-         })
-         .catch((e) => {
-           console.error(e)
-         })
+    Axios.get(`http://localhost:9100/pvs-api/project/1/${projectId}`)
+    .then((response) => {
+      setCurrentProject(response.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
   }, [])
+
+  useEffect(() => {
+    if(Object.keys(currentProject).length != 0) {
+      handleToggle()
+      const githubRepo = currentProject.repositoryDTOList.find(repo => repo.type == 'github')
+      const query = githubRepo.url.split("github.com/")[1]
+  
+        // todo need reafctor with async
+      Axios.get(`http://localhost:9100/pvs-api/issues/${query}`)
+        .then((response) => {
+          console.log(response.data)
+          setIssueListData(response.data)
+          handleClose()
+        })
+        .catch((e) => {
+          console.error(e)
+        })
+    }
+  }, [currentProject, prop.startMonth, prop.endMonth])
 
   useEffect(() => {
     const { endMonth } = prop
@@ -69,15 +99,16 @@ function IssuePage(prop) {
 
   return(
     <div style={{marginLeft:"10px"}}>
+       <Backdrop className={classes.backdrop} open={open}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <div className={classes.root}>
         <ProjectAvatar 
           size = "small" 
-          avatarURL={avatarURL}
-          projectId={projectId}
-          projectName={projectName}
+          project={currentProject}
         />
         <p>
-          <h2>{projectName}</h2>
+          <h2>{currentProject.projectName}</h2>
           
         </p>
       </div>
@@ -86,7 +117,7 @@ function IssuePage(prop) {
           <div>
             <h1>Team</h1>
             <div>
-              <DrawingBoard data={dataForIssueChart}/>
+              <DrawingBoard data={dataForIssueChart} color='skyblue'/>
             </div>
           </div>
         </div>
