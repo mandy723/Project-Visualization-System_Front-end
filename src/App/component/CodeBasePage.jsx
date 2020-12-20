f48c382d1dfe3e0fcb18ca7cc2e57e7ef9038f54
rvs-team <t109598097@ntut.org.tw>
2020-12-20 19:22:23 +0800

    ✨Implemented Code base chart

--- src/App/component/CodeBasePage.jsx
+++ src/App/component/CodeBasePage.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Card } from '@material-ui/core'
 import ProjectAvatar from './ProjectAvatar'
 import DrawingBoard from './DrawingBoard'
 import Axios from 'axios'
 import moment from 'moment'
-import CircularProgress from '@material-ui/core/CircularProgress'
+import { CircularProgress, Backdrop } from '@material-ui/core'
 import { connect } from 'react-redux';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,6 +17,10 @@ const useStyles = makeStyles((theme) => ({
       },
       minWidth: '30px',
     },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
 }))
 
 function CodeBasePage(prop) {
@@ -24,19 +28,50 @@ function CodeBasePage(prop) {
   const [commitListData, setCommitListData] = useState([])
   const [dataForCodeBaseChart, setDataForCodeBaseChart] = useState({ labels:[], data: {additions: [], deletions: []} })
 
+  const [currentProject, setCurrentProject] = useState({ })
+
   const projectId = localStorage.getItem("projectId")
-  const projectName = localStorage.getItem("projectName")
-  const avatarURL = localStorage.getItem("avatarURL")
+
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
 
   useEffect(() => {
-    Axios.get("http://localhost:9100/pvs-api/commits/facebook/react")
-         .then((response) => {
-           setCommitListData(response.data)
-         })
-         .catch((e) => {
-           console.error(e)
-         })
+    Axios.get(`http://localhost:9100/pvs-api/project/1/${projectId}`)
+    .then((response) => {
+      setCurrentProject(response.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
   }, [])
+
+  useEffect(() => {
+    if(Object.keys(currentProject).length != 0) {
+      handleToggle()
+      const githubRepo = currentProject.repositoryDTOList.find(repo => repo.type == 'github')
+      const query = githubRepo.url.split("github.com/")[1]
+      Axios.post(`http://localhost:9100/pvs-api/commits/${query}`)
+      .then((response) => {
+        // todo need reafctor with async
+        Axios.get(`http://localhost:9100/pvs-api/commits/${query}`)
+          .then((response) => {
+            setCommitListData(response.data)
+            handleClose()
+          })
+          .catch((e) => {
+            console.error(e)
+          })
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+    }
+  }, [currentProject, prop.startMonth, prop.endMonth])
   
   useEffect(() => {
     const { startMonth, endMonth } = prop
@@ -63,15 +98,16 @@ function CodeBasePage(prop) {
 
   return(
     <div style={{marginLeft:"10px"}}>
+       <Backdrop className={classes.backdrop} open={open}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <div className={classes.root}>
         <ProjectAvatar 
           size = "small" 
-          avatarURL={avatarURL}
-          projectId={projectId}
-          projectName={projectName}
+          project={currentProject}
         />
         <p>
-          <h2>{projectName}</h2>
+          <h2>{currentProject.projectName}</h2>
           
         </p>
       </div>
